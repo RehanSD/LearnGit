8a64e9aa81694c5a67e04cc9aad2e875f3caf16e
Rehan S. Durrani <rehansdurrani@gmail.com>
2017-01-06 11:20:44 -0800

    Added a statement to LearnGit.java

--- LearnGit.java
+++ LearnGit.java
@@ -1,5 +1,6 @@
 public class LearnGit{
   public static void main(String[] args){
     System.out.println("This is just a dummy file.");
+    System.out.println("This is an extra line added only to the dummybranch, for the purpose of merging.");
   }
 }
